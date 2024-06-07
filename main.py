4ee6052a6b1df98c83c21b5b9b769b95be08dc55
MokotoK <mokotok@protonmail.com>
2024-06-07 13:35:41 +1000

    A bunch of small bugs and code inefficiencies removed

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import sys
 from tkinter import ttk, messagebox, scrolledtext
-import tkinter as tk
 import database as data
+import tkinter as tk
+import sys
+
+# TODO: Sort entries in manage passwords, alphabetically
+# TODO: Add scroll bar in manage passwords treeview, so that you can see a longer list of password
+# TODO: Write up full doc strings
+# TODO: Write up Readme
+# TODO: Implement login window
+# TODO: Change visual design to look.... better..
 
 
-# TODO: Actually makes this something I like, because currently its design is ver undesirable.
-# TODO: Deal with unique website entries
 def add_password(event=None):
+    # Deals with empty fields case, all fields need to be filled or else database will throw error
     if website.get() == "" or email.get() == "" or password.get() == "":
         return messagebox.showinfo("Missing fields", "Information missing")
+
+    # Deals with unique website case, each website must be different or else database throws unique error, websites also
+    # Need to be unique as we use them for keeping track of entries, editing and deleting.
+    with data.app.app_context():
+        db_entries = data.db.session.scalars(data.db.select(data.Passwords)).all()
+        websites = [entry.website for entry in db_entries]
+        if website.get() in websites:
+            return messagebox.showinfo("Already exists", f"An email and password already exists for {website.get()}")
 
     new_entry = data.Passwords(
         website=website.get().lower(),
@@ -21,84 +35,10 @@ def add_password(event=None):
 
         # Clears the inputs after commiting a password
         website.delete(0, tk.END)
-        email.delete(0, tk.END)
+        # email.delete(0, tk.END)
         password.delete(0, tk.END)
 
 
-# TODO: GIGA SPAGHETTI CODES!!!! NEEDS SERIOUS REFACTORING, CLEANING, COMBINING... But... it works at the moment
-# TODO: Handle when nothing is selected and user clicks on edit
-# TODO: Add edit password feature
-def edit(id):
-    if id == '':
-        return messagebox.showinfo("Missing fields", "Information missing")
-
-    # TODO: Find a way not to destory the window everytime I confirm a edit, but just hide the window, like the how the
-    # window operates.
-    def second_event(event=None):
-        with data.app.app_context():
-            entry = data.db.session.scalar(data.db.select(data.Passwords).where(data.Passwords.website == id[0]))
-            entry.id = entry.id
-            entry.website = website.get()
-            entry.email = email.get()
-            entry.password = password.get()
-            data.db.session.commit()
-            window.destroy()
-            return manage_passwords()
-
-    # Destroys edit window and replaces it with manager window
-    def edit_event():
-        window.destroy()
-        return manage_passwords()
-
-    disable_event()
-    window = tk.Toplevel()
-    frame = tk.Frame(window, pady=5, padx=5)
-    frame.grid(column=0, row=0)
-
-    tk.Label(frame, text="Edit or Delete Entry").grid(column=1, row=2, sticky="W E")
-    tk.Label(frame, text="Website").grid(column=0, row=3)
-    tk.Label(frame, text="Email").grid(column=1, row=3)
-    tk.Label(frame, text="Password").grid(column=2, row=3)
-
-    website = tk.Entry(frame)
-    email = tk.Entry(frame)
-    password = tk.Entry(frame)
-
-    website.insert(tk.END, id[0])
-    email.insert(tk.END, id[1])
-    password.insert(tk.END, id[2])
-
-    website.grid(column=0, row=4)
-    email.grid(column=1, row=4)
-    password.grid(column=2, row=4)
-
-    ttk.Button(frame, text="Confirm", command=second_event).grid(column=0, row=5)
-
-    window.protocol("WM_DELETE_WINDOW", edit_event)
-    window.bind('<Return>', second_event)
-
-# TODO: Hand the case when delete is clicked but an entry isnt selected.
-def delete(website):
-    if id == '':
-        return messagebox.showinfo("Missing fields", "Information missing")
-
-    with data.app.app_context():
-        entry = data.db.session.scalar(data.db.select(data.Passwords).where(data.Passwords.website == website))
-        data.db.session.delete(entry)
-        data.db.session.commit()
-
-        disable_event()
-        manage_passwords()
-
-
-# Prevents multiple instances of the "Manage passwords" window from appearing by hiding the window on exit. Without this
-# If a user was to click the manage passwords button while the window was already up it would open another window
-def disable_event():
-    window.withdraw()
-    pass
-
-
-# TODO: once this is fully functional, think of a way to imporve its design... its labouress at the moment.
 def manage_passwords():
     """Reads, Updates and Deletes Passwords"""
 
@@ -114,14 +54,87 @@ def manage_passwords():
     tree_box.heading('2', text="Email")
     tree_box.heading('3', text="Password")
 
-    # TODO: SORT passwords to display in alphabetical order
     # Reads all entries in the passwords database and inputs them all into a scrollable text box on screen.
     with data.app.app_context():
         for entry in data.db.session.scalars(data.db.select(data.Passwords)).all():
             tree_box.insert('', 'end', values=(entry.website, entry.email, entry.password))
 
-    ttk.Button(frame, text="Edit", command=lambda: edit(tree_box.item(tree_box.focus())['values'])).grid(column=0, row=5)
-    ttk.Button(frame, text="Delete", command=lambda: delete(tree_box.item(tree_box.focus())['values'][0])).grid(column=2, row=5)
+    (ttk.Button(frame, text="Edit", command=lambda: edit_selection(tree_box.item(tree_box.focus())['values']))
+     .grid(column=0, row=5))
+    (ttk.Button(frame, text="Delete", command=lambda: delete(tree_box.item(tree_box.focus())['values']))
+     .grid(column=2, row=5))
+
+
+# Prevents multiple instances of the "Manage passwords" window from appearing by hiding the window on exit. Without this
+# If a user was to click the manage passwords button while the window was already up it would open another window
+def manage_passwords_event():
+    window.withdraw()
+    pass
+
+
+def edit_selection(selected):
+    if selected == '':
+        return messagebox.showinfo("Nothing Selected", "Select an entry to edit")
+
+    manage_passwords_event()
+    edit_window.deiconify()
+    frame = tk.Frame(edit_window, pady=5, padx=5)
+    frame.grid(column=0, row=0)
+
+    tk.Label(frame, text="Edit or Delete Entry").grid(column=1, row=2, sticky="W E")
+    tk.Label(frame, text="Website").grid(column=0, row=3)
+    tk.Label(frame, text="Email").grid(column=1, row=3)
+    tk.Label(frame, text="Password").grid(column=2, row=3)
+
+    edit_website = tk.Entry(frame)
+    edit_email = tk.Entry(frame)
+    edit_password = tk.Entry(frame)
+
+    edit_website.insert(tk.END, selected[0])
+    edit_email.insert(tk.END, selected[1])
+    edit_password.insert(tk.END, selected[2])
+
+    edit_website.grid(column=0, row=4)
+    edit_email.grid(column=1, row=4)
+    edit_password.grid(column=2, row=4)
+
+    (ttk.Button(frame, text="Confirm", command=lambda: confirm_edit(selected, edit_website, edit_email, edit_password))
+     .grid(column=0, row=5))
+
+    edit_window.bind('<Return>', lambda event: confirm_edit(selected, edit_website, edit_email, edit_password))
+    edit_window.protocol("WM_DELETE_WINDOW", edit_window_event)
+
+
+# window operates.
+def confirm_edit(selected, edit_website, edit_email, edit_password, event=None):
+    with data.app.app_context():
+        entry = data.db.session.scalar(data.db.select(data.Passwords).where(data.Passwords.website == selected[0]))
+        entry.id = entry.id
+        entry.website = edit_website.get()
+        entry.email = edit_email.get()
+        entry.password = edit_password.get()
+        data.db.session.commit()
+        return edit_window_event()
+
+
+# hides edit window and un hides manager window, this is a better option then destroying and re-creating these windows
+def edit_window_event():
+    edit_window.withdraw()
+    return manage_passwords()
+
+
+def delete(selected):
+    if selected == '':
+        return messagebox.showinfo("No selection", "No entry selected")
+
+    with data.app.app_context():
+        entry = data.db.session.scalar(data.db.select(data.Passwords).where(data.Passwords.website == selected[0]))
+        data.db.session.delete(entry)
+        data.db.session.commit()
+
+        manage_passwords_event()
+        manage_passwords()
+
 
 # For login features, currently disabled while building out the database functions
 # def login():
@@ -140,7 +153,8 @@ def manage_passwords():
 root = tk.Tk()
 window = tk.Toplevel(root)
 window.withdraw()
-
+edit_window = tk.Toplevel(root)
+edit_window.withdraw()
 # top = tk.Toplevel()
 # top.title("Login to MK-Pass")
 root.title("MK - Password Manager")
@@ -167,7 +181,7 @@ root.title("MK - Password Manager")
 # MAIN WINDOW------------------------------------------
 canvas = tk.Canvas(height=555, width=700)
 canvas.grid(column=0, row=0)
-image = tk.PhotoImage(file='') #mk-logo-copy.png
+image = tk.PhotoImage(file='mk-logo-copy.png')
 canvas.create_image(350, 270, image=image)
 
 mainframe = ttk.Frame(root, width=1000, height=1000, padding=20)
@@ -192,5 +206,5 @@ ttk.Button(mainframe, text="Passwords", command=manage_passwords).grid(column=2,
 
 # Lets you confirm an entry with the enter key
 root.bind('<Return>', add_password)
-window.protocol("WM_DELETE_WINDOW", disable_event)
+window.protocol("WM_DELETE_WINDOW", manage_passwords_event)
 root.mainloop()
